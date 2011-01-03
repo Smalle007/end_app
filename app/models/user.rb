786f9ac3b3c3e0f4ca48d7ff7b53c1108c6d98b3
Smalle007 <geofkeput@hotmail.com>
2011-01-03 14:35:18 +0100

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+
+	validates :first_name, :last_name, :email, :presence => true
+	validates :email, :uniqueness => true
+	validates :password, :presence => true, :length => { :within => 6..12}
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EndApp::Application.config.secret_token = 'b415bfbfcd1c02eee819ba2035565b986841cf9b24e98e0d409990fe502dd28ab718ba5adcb50c68bdeaa2dea56d0d24f6d81ed630de29ee8ac25d0e5b330193'

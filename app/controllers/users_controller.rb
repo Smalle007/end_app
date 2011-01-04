124f10d841f5984fce8a53ed6305938a65f8d00a
Smalle007 <geofkeput@hotmail.com>
2011-01-04 22:15:14 +0100

    last commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -113,16 +113,11 @@ class UsersController < ApplicationController
   end
   
   def authenticate
-	#User.new(params[:userform]) will create a new object of User, retrieve values from the form and store it variable @user.
 	@user = User.new(params[:userform])
-	#find records with username,password
 	valid_user = User.find(:first,:conditions => ["first_name = ? and password = ?",@user.first_name, @user.password])
 
-	#if statement checks whether valid_user exists or not
 	if valid_user
-	#creates a session with username
 		session[:user_id]=valid_user.first_name
-	#redirects the user to our private page.
 		redirect_to :action => 'index'
 	else
 		flash[:notice] = "Invalid User/Password"
